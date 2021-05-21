a365f58c2ded448d5d0515eda2caa7b9ca59ab69
jbonofre <jbonofre@apache.org>
2021-05-21 09:50:32 +0200

    Update

--- activemq-bookkeeper-store/src/test/java/org/apache/activemq/bookkeeper/store/SimpleTest.java
+++ activemq-bookkeeper-store/src/test/java/org/apache/activemq/bookkeeper/store/SimpleTest.java
@@ -1,24 +1,44 @@
 package org.apache.activemq.bookkeeper.store;
 
 import org.apache.bookkeeper.client.BookKeeper;
+import org.apache.bookkeeper.client.LedgerEntry;
 import org.apache.bookkeeper.client.LedgerHandle;
+import org.apache.bookkeeper.conf.ClientConfiguration;
 import org.junit.Test;
+
+import java.util.Enumeration;
 
 public class SimpleTest {
 
     @Test
     public void simpleStorage() throws Exception {
-        // ClientConfiguration clientConfiguration = new ClientConfiguration();
-        // clientConfiguration.setMetadataServiceUri("127.0.0.1:2181");
-        // clientConfiguration.setAddEntryTimeout(2000);
-        BookKeeper bookKeeper = new BookKeeper("127.0.0.1:2181");
+        ClientConfiguration config = new ClientConfiguration();
+        config.setMetadataServiceUri("zk+null://localhost:2181");
+        config.setAddEntryTimeout(2000);
 
-        byte[] messages = "messages".getBytes();
-        LedgerHandle ledger = bookKeeper.createLedger(BookKeeper.DigestType.MAC, messages);
+        try (BookKeeper bookKeeper = new BookKeeper(config)) {
+            
+            byte[] messages = "messages".getBytes();
+            LedgerHandle ledger = bookKeeper.createLedger(BookKeeper.DigestType.MAC, messages);
+            long ledgerId = ledger.getId();
 
-        long entryId = ledger.addEntry("My First Message".getBytes());
+            ledger.addEntry("My First Message".getBytes());
 
-        System.out.println("Ledger size: " + ledger.getLength());
+            ledger.close();
+
+            ledger = bookKeeper.openLedger(ledgerId, BookKeeper.DigestType.MAC, messages);
+
+            Enumeration<LedgerEntry> entries = ledger.readEntries(0, ledger.getLastAddConfirmed());
+            while (entries.hasMoreElements()) {
+                LedgerEntry entry = entries.nextElement();
+                System.out.println("Entry: " + entry.getEntryId());
+            }
+
+            System.out.println("Ledger size: " + ledger.getLength());
+
+            ledger.close();
+            bookKeeper.close();
+        }
     }
 
 }
